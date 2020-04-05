34ac1eaca15cab2af38423225fbcf2e9d707c1e8
Dharmesh Patel <dharmeshec5@gmail.com>
2020-04-05 14:18:47 +0530

    - Project created

---
+++ Product/Product/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Product
+//
+//  Created by Dharmesh Patel on 05/04/20.
+//  Copyright © 2020 Dharmesh Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
